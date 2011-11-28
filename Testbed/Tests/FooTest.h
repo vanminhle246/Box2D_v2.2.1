7627c016455c577d6911dd092a97887b093a2f5a
Van-Minh Le <vanminh.le246@gmail.com>
2011-11-28 14:03:26 +0700

    Update 20111128h1403

--- Testbed/Tests/FooTest.h
+++ Testbed/Tests/FooTest.h
@@ -1,6 +1,6 @@
 
 #pragma once
-#include "e:\jtn\box2d\box2d_v2.2.1\testbed\framework\test.h"
+#include "..\framework\test.h"
 #define DEGTORAD 0.0174532925199432957f
 #define RADTODEG 57.295779513082320876f
 
